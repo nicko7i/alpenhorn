421b931e4342aaa860f1b6e29bb0c19ccc0fc21a
Nick Okasinski <nicko7i@gmail.com>
2018-12-01 18:56:32 -0800

    machine, distro, board support package and Yocto branch can now be specified as environment variables.

--- build/source-me-first.sh
+++ build/source-me-first.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+export MACHINE=${ALPENHORN_MACHINE:-imx6sllevk}
+export DISTRO=${ALPENHORN_DISTRO:-fslc-framebuffer}
+VARIANT=${ALPENHORN_VARIANT:-core-image-minimal}
+BSP=${ALPENHORN_BSP_URL:-http://github.com/boundarydevices/boundary-bsp-platform}
+BSP_BRANCH=${ALPENHORN_BSP_BRANCH:-pyro}
+
+alias setup="source setup-environment ${VARIANT}"
+
 function repo_init {
   #
   #  Initialize the git repository if none is present
   [ -d .repo ] && { echo repo already initialized, skipping...; return 0; }
   #
   #  Update this line as necessary for your project.
-  yes | repo init -u http://github.com/boundarydevices/boundary-bsp-platform -b pyro
+  yes | repo init -u ${BSP} -b ${BSP_BRANCH}
 }
 
 function repo_sync {
@@ -16,23 +24,21 @@ function repo_sync {
   sed -i '/CONNECTIVITY_CHECK_URIS/cCONNECTIVITY_CHECK_URIS=""' conf/local.conf
 }
 
-function setup_environment {
-  #
-  #  Update this line as necessary for your project.
-  MACHINE=nitrogen6x DISTRO=fslc-framebuffer \
-  source setup-environment core-image-minimal
-}
-
 function bake_bits {
-  bitbake core-image-minimal
+  for i in $(find .. -name '*sanity.bbclass'); do
+    sed -i '/Do not use Bitbake as root/s/raise_sanity_error/pass # /' ${i}
+  done
+  bitbake ${VARIANT}
 }
 
-function do_it_all {
-  repo_init
-  repo_sync
-  setup_environment
-  fixup_environment
-  bake_bits
+function usage {
+  echo Usage:  In the /build directory:
+  echo "  repo_init"
+  echo "  repo_sync"
+  echo "  setup"
+  echo "  bake_bits"
+  echo
+  echo "The setup script changes your directory to ${VARIANT}"
 }
-alias setup='MACHINE=nitrogen6x DISTRO=fslc-framebuffer \
-  source setup-environment core-image-minimal'
+
+usage
